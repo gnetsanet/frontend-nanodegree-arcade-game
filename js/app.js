6851272c50dbeba9421426a3c0863b0114a4baaf
Netsanet Gebremedhin <gnetsanet@gmail.com>
2019-01-06 11:03:58 -0500

    Use proper ES6 class syntax.

--- js/app.js
+++ js/app.js
@@ -1,16 +1,39 @@
 'use strict';
 
 // Enemies our player must avoid
-var Enemy = function(speed, x, y) {
+class Enemy {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
-    this.sprite = 'images/enemy-bug.png';
-    this.speed = speed;
-    this.x = x;
-    this.y = y;
+    constructor(speed, x, y)  {
+        this.sprite = 'images/enemy-bug.png';
+        this.speed = speed;
+        this.x = x;
+        this.y = y;
+    }
+
+    // Update the enemy's position, required method for game
+    // Parameter: dt, a time delta between ticks
+    update(dt) {
+        // You should multiply any movement by the dt parameter
+        // which will ensure the game runs at the same speed for
+        // all computers.
+        this.x = this.x + this.speed * dt;
+
+        // Keep the enemy within the canvas and have it come back to its original starting x-axis position
+        if(this.x >= 505) {
+            this.x = 0;
+        }
+
+        contactWithEnemy(this);
+    };
+
+    // Draw the enemy on the screen, required method for game
+    render() {
+        ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    };
 };
 
 var Player = function(speed, x, y) {
@@ -107,27 +130,6 @@ var checkIfGameIsWon = function() {
 Player.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
     checkIfGameIsWon();
-};
-
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
-Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
-    this.x = this.x + this.speed * dt;
-
-    // Keep the enemy within the canvas and have it come back to its original starting x-axis position
-    if(this.x >= 505) {
-        this.x = 0;
-    }
-
-    contactWithEnemy(this);
-};
-
-// Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
 // Now write your own player class
